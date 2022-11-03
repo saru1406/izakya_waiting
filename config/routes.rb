2c5d75d847e7eb6d8dccb610676f356713958b1e
EC2 Default User <ec2-user@ip-172-31-8-59.ap-northeast-1.compute.internal>
2022-11-03 19:12:38 +0000

    device update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+ # 顧客用
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 店舗用
+devise_for :stores,skip: [:passwords], controllers: {
+  registrations: "public_store/registrations",
+  sessions: 'public_store/sessions'
+}
+
+# 管理者用
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+root to: 'public/homes#top'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
