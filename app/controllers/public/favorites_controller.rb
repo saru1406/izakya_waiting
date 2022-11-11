27f5fbcb299da1b9e024eed0e2751063989821cd
EC2 Default User <110827986+saru1406@users.noreply.github.com>
2022-11-11 21:24:11 +0000

    comment,favorite非同期

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,18 +1,16 @@
 class Public::FavoritesController < ApplicationController
 
   def create
-    store = Store.find(params[:store_id])
-    review = Review.find(params[:review_id])
-    favorite = current_customer.favorites.new(review_id: review.id, store_id: store.id)
+    @store = Store.find(params[:store_id])
+    @review = Review.find(params[:review_id])
+    favorite = current_customer.favorites.new(review_id: @review.id, store_id: @store.id)
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
-    store = Store.find(params[:store_id])
-    review = Review.find(params[:review_id])
-    favorite = current_customer.favorites.find_by(review_id: review.id, store_id: store.id)
+    @store = Store.find(params[:store_id])
+    @review = Review.find(params[:review_id])
+    favorite = current_customer.favorites.find_by(review_id: @review.id, store_id: @store.id)
     favorite.destroy
-    redirect_to request.referer
   end
 end
--- app/controllers/public/review_comments_controller.rb
+++ app/controllers/public/review_comments_controller.rb
@@ -1,25 +1,21 @@
 class Public::ReviewCommentsController < ApplicationController
 
   def create
-    store = Store.find(params[:store_id])
-    review = Review.find(params[:review_id])
-    review_comment =ReviewComment.new(review_comment_params)
-    review_comment.customer_id = current_customer.id
-    review_comment.review_id = review.id
-    review_comment.store_id = store.id
-    if review_comment.save
-      redirect_to store_review_path(store.id,review.id)
-    else
-      render :reviews/show
-    end
+    @store = Store.find(params[:store_id])
+    @review = Review.find(params[:review_id])
+    @review_comments = @review.review_comments.all
+    @review_comment = ReviewComment.new(review_comment_params)
+    @review_comment.customer_id = current_customer.id
+    @review_comment.review_id = @review.id
+    @review_comment.store_id = @store.id
+    @review_comment.save
   end
 
   def destroy
-    review_comment = ReviewComment.find(params[:id])
-    review_comment.destroy
-    store = Store.find(params[:store_id])
-    review = Review.find(params[:review_id])
-    redirect_to store_review_path(store.id,review.id)
+    @review = Review.find(params[:review_id])
+    @review_comment = ReviewComment.find(params[:id])
+    @review_comments = @review.review_comments.all
+    @review_comment.destroy
   end
 
   def index
