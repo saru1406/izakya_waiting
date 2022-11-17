4d04a056e5065d76d9b9c9e0053ceaf4ae6a667b
EC2 Default User <110827986+saru1406@users.noreply.github.com>
2022-11-17 18:33:34 +0000

    devise error 日本語化

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module IzakayaWaiting
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+     #app/config/locales/ja.ymlの読み込み
+    config.i18n.default_locale = :ja
+
   end
 end
