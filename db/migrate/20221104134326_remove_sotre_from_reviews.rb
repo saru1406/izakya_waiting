c083af6512cdae4c58f11afc71478213d3b421b4
EC2 Default User <110827986+saru1406@users.noreply.github.com>
2022-11-21 05:17:29 +0000

    migrate update

--- db/migrate/20221104134326_remove_sotre_from_reviews.rb
+++
@@ -1,5 +0,0 @@
-class RemoveSotreFromReviews < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :reviews, :sotre, :integer
-  end
-end
--- db/migrate/20221104134514_remove_sotre_id_from_reviews.rb
+++
@@ -1,5 +0,0 @@
-class RemoveSotreIdFromReviews < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :reviews, :sotre_id, :integer
-  end
-end
