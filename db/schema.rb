c14becab067ea8b4212d3ad4813dda859cea4d87
EC2 Default User <110827986+saru1406@users.noreply.github.com>
2022-11-21 10:29:40 +0000

    db:teble update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_15_044931) do
+ActiveRecord::Schema.define(version: 2022_11_21_102754) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
